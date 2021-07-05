21b8a8cd24a8b690c0e9d5eb5523e0df6b8d2e57
ShreyasNraj <shreyasnraj143@gmail.com>
2021-07-05 23:59:09 +0530

    before button -

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,24 @@
+import CartIcon from '../Cart/CartIcon';
+import {useContext} from 'react'
+import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context'
+
+const HeaderCartButton = (props) => {
+const cartCtx= useContext(CartContext);
+
+const numberOfCartItems = cartCtx.items.reduce((curNumber,item) => {
+  return curNumber +item.amount;
+},0);
+
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Bids</span>
+  <span className={classes.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
+
+export default HeaderCartButton;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,18 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Welcome to Our Auction Page</h2>
+      <p>
+        blah vlah
+      </p>
+      <p>
+      blah vlah
+      blah vlah!
+      </p>
+    </section>
+  );
+};
+
+export default MealsSummary;
